0ef2e88959515d1aa816c871101bac655ad20fa1
GeMixl <g.michl@gmx.net>
2021-01-04 17:07:16 +0100

    Very pythonic version of aoc_2020_day1

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
+import itertools
+
 
 def aoc_2020_day1():
-
-    with open('./input.txt', 'r') as file:
-        data = [int(line) for line in file]
-
-    for k, i in enumerate(data):
-        for j in data[k+1:]:
-            if i+j == 2020:
-                print("{} * {} = {}".format(i, j, i*j))
+    for (a, b) in itertools.combinations([int(n) for n in open('input.txt')], 2):
+        if a + b == 2020:
+            print(a * b)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     aoc_2020_day1()
 
